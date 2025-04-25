23fa5219d1a0fe73424dd7c901a65411489b9b17
Abdul Rafay <rafayh74@gmail.com>
2025-04-25 14:24:09 +0500

    finalised

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import { userController } from '../controllers/user.controller';
+import { isAuthenticated } from '~/middlewares/auth.middleware';
 
 
 const authRoutes = express.Router();
 authRoutes.post("/register", userController.register);
+authRoutes.put("/profile", isAuthenticated, userController.updateProfile); 
 
 
 export default authRoutes
